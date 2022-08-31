f2eaf6324e21378c6a24ee9dc4973c23315a8c75
Krasnyansky Alexandr <krasnyansky.alexandr@gmail.com>
2022-08-31 15:44:29 +0300

    feat: validate

--- scripts/validate.js
+++ scripts/validate.js
@@ -74,11 +74,13 @@ const enableValidation = () => {
 
 
 // включить валидацию
+enableValidation();
+
 enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__submit-button',
-    inactiveButtonClass: 'popup__submit-button_type_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_type_active'
+        formSelector: '.popup__form',
+        inputSelector: '.popup__input',
+        submitButtonSelector: '.popup__submit-button',
+        inactiveButtonClass: 'popup__submit-button_type_disabled',
+        inputErrorClass: 'popup__input_type_error',
+        errorClass: 'popup__error_type_active'
 });
